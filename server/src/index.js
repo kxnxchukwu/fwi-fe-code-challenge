fa87e4fa78a163e51fde632527d28975bf1d272a
Stas Danishevsky <stas.danishevsky@fourwindsinteractive.com>
2019-03-28 13:42:28 -0600

    Added initial data creation on app start.

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 console.log('Starting server...');
 
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const playersRoute = require('./routes/players');
@@ -10,6 +12,18 @@ app.use(bodyParser.json());
 
 // Routes
 app.use(ROUTE_PLAYERS, playersRoute);
+
+// Creating initial data set if it doesn't exist
+const initialDataFile = path.join(__dirname, '..', 'initialData.json');
+const dataDir = path.join(__dirname, '..', 'data');
+const dataFile = path.join(dataDir, 'index.json');
+if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir);
+}
+if (!fs.existsSync(dataFile)) {
+    fs.copyFileSync(initialDataFile, dataFile);
+    fs.chmodSync(dataFile, 777);
+}
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
