151e83923ed6744deae2a90158f282a7c03e06e0
Mikkel Laursen <mlaursen03@gmail.com>
2020-07-13 10:06:33 -0600

    feat: Updated PlayerTable to use react-redux hooks

--- client/src/PlayerTable/PlayerTable.jsx
+++ client/src/PlayerTable/PlayerTable.jsx
@@ -1,78 +1,42 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connectAdvanced } from 'react-redux';
-import shallowEqual from 'shallowequal';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { COUNTRIES } from '../constants';
 import { fetchPlayersSuccess } from '../appState/actions';
 
 import './PlayerTable.scss';
 import TableHeader from './TableHeader';
 import TableBody from './TableBody';
 
-class PlayerTable extends PureComponent {
-  static propTypes = {
-    players: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        country: PropTypes.oneOf(Object.keys(COUNTRIES)),
-        winnings: PropTypes.number.isRequired,
-        imageUrl: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-    fetchPlayersSuccess: PropTypes.func.isRequired,
-  };
+const getPlayers = (state) => state.playerIds.map((id) => state.players[id]);
 
-  componentDidMount() {
-    const { fetchPlayersSuccess } = this.props;
-    fetch('http://localhost:3001/players', {
-      headers: {
-        Accept: 'application/json',
-      },
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        if (data) {
-          fetchPlayersSuccess(data);
-          return data;
-        }
-        throw new Error(data.message);
+const PlayerTable = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    (async function getPlayers() {
+      const response = await fetch('http://localhost:3001/players', {
+        headers: {
+          Accept: 'application/json',
+        },
       });
-  }
 
-  render() {
-    const { players } = this.props;
-    return (
-      <div
-        id="player-table-grid"
-        role="grid"
-        aria-label="Poker Players"
-        className="player-table"
-      >
-        <TableHeader />
-        <TableBody players={players} />
-      </div>
-    );
-  }
-}
+      const json = await response.json();
+      dispatch(fetchPlayersSuccess(json));
+    })();
+  }, [dispatch]);
 
-export default connectAdvanced((dispatch) => {
-  let result;
-  const actions = bindActionCreators({ fetchPlayersSuccess }, dispatch);
+  const players = useSelector(getPlayers);
 
-  return (state, props) => {
-    const players = state.playerIds.map((id) => state.players[id]);
+  return (
+    <div
+      id="player-table-grid"
+      role="grid"
+      aria-label="Poker Players"
+      className="player-table"
+    >
+      <TableHeader />
+      <TableBody players={players} />
+    </div>
+  );
+};
 
-    const nextResult = { ...props, ...actions, players };
-
-    if (!shallowEqual(result, nextResult)) {
-      result = nextResult;
-    }
-
-    return result;
-  };
-})(PlayerTable);
+export default PlayerTable;
