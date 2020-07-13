aee59c52f19419476aabd2e23b9a78c04006bd7f
Mikkel Laursen <mlaursen03@gmail.com>
2020-07-13 09:42:49 -0600

    chore: Updated client for latest create-react-app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import Header from './Header/Header';
 import PlayerTable from './PlayerTable/PlayerTable';
 
 const App = () => {
   return (
-    <Fragment>
+    <>
       <Header />
       <PlayerTable />
-    </Fragment>
+    </>
   );
 };
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -14,9 +14,11 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
   document.getElementById('root')
 );
 
